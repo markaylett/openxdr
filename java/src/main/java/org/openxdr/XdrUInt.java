966bddba79285b7d7a1c6eed8a896151d5dddf9f
Mark Aylett <mark.aylett@gmail.com>
2010-05-21 18:36:11 +0100

    Unsigned int checks.

---
+++ java/src/main/java/org/openxdr/XdrUInt.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2010 Mark Aylett <mark.aylett@gmail.com>
+ * 
+ * The contents of this file are subject to the Common Development and
+ * Distribution License (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.sun.com/cddl/
+ * 
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ */
+package org.openxdr;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+public final class XdrUInt {
+
+    private XdrUInt() {
+    }
+
+    public static void encode(ByteBuffer buf, int val) {
+        assert ByteOrder.BIG_ENDIAN == buf.order();
+        if (val < 0)
+            throw new IllegalArgumentException();
+        buf.putInt(val);
+    }
+
+    public static int decode(ByteBuffer buf) {
+        assert ByteOrder.BIG_ENDIAN == buf.order();
+        final int val = buf.getInt();
+        if (val < 0)
+            throw new IllegalArgumentException();
+        return val;
+    }
+
+    public static final Codec<Integer> CODEC = new Codec<Integer>() {
+        public final void encode(ByteBuffer buf, Integer val) {
+            XdrUInt.encode(buf, val);
+        }
+
+        public final Integer decode(ByteBuffer buf) {
+            return XdrUInt.decode(buf);
+        }
+    };
+}
